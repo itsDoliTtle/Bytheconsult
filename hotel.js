ea7ffd89e4842d5e1622b9cb4224f0f128614b6f
Yonnilight <yonnilight@gmail.com>
2024-02-21 16:01:23 +1300

    bythe v1

---
+++ hotel.js
@@ -0,0 +1,9 @@
+
+    function img(anything) {
+      document.querySelector('.slide').src = anything;
+    }
+
+    function change(change) {
+      const line = document.querySelector('.image');
+      line.style.background = change;
+    }
